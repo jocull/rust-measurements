8bad24e8dbf7a1fa63e84ce0126ddb97d0922857
James O'Cull <jocull@delmarsd.com>
2015-06-16 22:07:19 -0400

    Okay, clean pages for real this time

---
+++ target/doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['measurements'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='measurements/length/struct.Length.html' title='measurements::length::Length'>Length</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ target/doc/implementors/core/ops/trait.Div.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['measurements'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Div.html' title='core::ops::Div'>Div</a> for <a class='struct' href='measurements/length/struct.Length.html' title='measurements::length::Length'>Length</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
