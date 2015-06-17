65b471d42bb58cf88e5717ede9bf359751b5d105
James O'Cull <jocull@delmarsd.com>
2015-06-16 22:03:46 -0400

    I really botched that whole pages thing, huh...

--- target/doc/measurements/length/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"mod":[["consts",""]],"struct":[["Length","The `Length` struct can be used to deal with lengths in a common way. Common metric and imperial units are supported."]]});
